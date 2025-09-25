b269125665de78bcf22db64e3a332d6aff05823e
Vitaliy-do <4810672@gmail.com>
2025-09-25 22:06:52 +0300

    Update urls.py

--- first_project/urls.py
+++ first_project/urls.py
@@ -26,7 +26,4 @@ urlpatterns = [
     path('current_time/', time_view, name='time'),
     path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
-    path('', home_view),
-    path('time/', time_view),
-    path('workdir/', workdir_view),
 ]
